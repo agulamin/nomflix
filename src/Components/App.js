63ae3726b2539d7267bb35b3a5d9086a97476682
youngmin9 <agulamin@gmail.com>
2019-12-11 17:38:43 +0900

    #3.2 CSS in React part Three

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import Header from "Components/Header";
 import Router from "Components/Router";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
+        
         <Router />
       </>
     );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header"
 
 
 export default () => (
     <Router>
+        <>
+        <Header />
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/tv" exact component={TV} />
-            <Route path="/tv/popular" render={()=><h1>popular</h1>} />
             <Route path="/search" component={Search} />
             <Redirect from="*" to="/" />
         </Switch>
+        </>
     </Router>
 );
